668bb842b21343fdc448542684aed3a08ea3471d
nespinoza <nestor.espinozap@gmail.com>
2015-12-10 19:00:57 -0300

    Added corrections.

--- plot_images.py
+++ plot_images.py
@@ -14,7 +14,7 @@ image_fnames = glob.glob(foldername+'Nestor1_00*.fit')
 # Get the darks:
 dark_frames = glob.glob(foldername+'darkN200*.fit')
 # Get sky flats:
-sky_frames = glob.glob(foldername+'skyflat200*.fit')
+sky_frames = glob.glob(foldername+'skyflat00*.fit')
 
 # Create master dark:
 for i in range(len(dark_frames)):
@@ -22,31 +22,46 @@ for i in range(len(dark_frames)):
     if i == 0:
         dark_images = np.copy(data)
     else:
-        dark_images.dstack((dark_images,np.copy(data)))
+        dark_images = np.dstack((dark_images,np.copy(data)))
 
-median_dark = np.median(dark_image,axis=2)
-
+median_dark = np.median(dark_images,axis=2)
 # Create master sky:
-for i in range(len(dark_frames)):
+for i in range(len(sky_frames)):
     data,h = pyfits.getdata(sky_frames[i],header=True)
     data = data - median_dark
     if i == 0:
         sky_images = np.copy(data/np.median(data))
     else:
-        sky_images.dstack((sky_images,np.copy(data/np.median(data))))
+        sky_images = np.dstack((sky_images,np.copy(data/np.median(data))))
 
-median_sky = np.median(sky_image,axis=2)
+# Correct for zeros on sky image:
+median_sky = np.median(sky_images,axis=2)
+pyfits.PrimaryHDU(median_sky).writeto('flat_field.fits')
+x_z,y_z = np.where(median_sky == 0)
+for i in range(len(x_z)):
+    median_sky[x_z,y_z] = 1.0
 
+# Now create median image from all the science frames:
 all_images = [] 
 for i in range(len(image_fnames)):
     data,h = pyfits.getdata(image_fnames[i],header=True)
+    pyfits.PrimaryHDU(data/np.median(data)).writeto('original.fits')
+    data = (data-median_dark)/median_sky
+    pyfits.PrimaryHDU(data/np.median(data)).writeto('corrected.fits')
+    sys.exit()
     if i == 0:
-        all_data = (data-median_dark)/median_sky
+        all_data = np.copy(data)
     else:
-        all_data = np.dstack((all_data,(data-median_dark)/median_sky))
+        all_data = np.dstack((all_data,np.copy(data)))
     all_images.append(np.copy(data))
 
 median_image = np.median(all_data,axis=2)
+
+# Plot it:
+im = plt.imshow(median_image)
+plt.show()
+
+# Graph every image:
 for i in range(len(all_images)):
     diff_image = all_images[i]-median_image
     im = plt.imshow(diff_image)
